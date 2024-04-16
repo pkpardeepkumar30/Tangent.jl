d8c3155a51cef74fba43b5c6fbe6389a8837061b
Pardeep Kumar <pardeep.iitb@gmail.com>
2024-04-16 14:20:32 +0200

    Minor change in document

--- src/numerics.jl
+++ src/numerics.jl
@@ -4,11 +4,11 @@
 
 Return the derivateive of a function ``f(x)`` at ``x``  
 ## Examples
-``f'(x_0) \\approx \\frac{f(x_0 + ϵ) - f(x)}{ϵ}``
+``f'(x_0) \\approx \\frac{f(x_0 + ϵ) - f(x)}{ϵ}
 julia > f()
 ``  
 """
-function approx_derivative(f, x::Float64; ϵ::Float64 = 1e-16)
+function approx_derivative(f, x; ϵ = 1e-16)
     f′ =  (f(x + ϵ) - f(x))/ϵ
     return f′
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using Tangent
 using Test
 
-@testset "Tangent.jl" begin
+@testset "Numerical Derivative" begin
     # Write your tests here.
+
+    f(x) = 2x + 1
+    
+    @test approx_derivative(f, 3.0; ϵ = 1.0) == 2.0
+    @test approx_derivative(f, 3.0; ϵ = 1.0) == 2.0
+    @test approx_derivative(f, 3.0; ϵ = 1.0) ≈ 2.0
+
 end
